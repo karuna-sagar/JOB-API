434f4bd7de43ac3e3f4403c472493ad613ffca8a
Karuna sagar <karunasagar129130@gmail.com>
2023-10-15 14:35:27 +0530

    get job controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -4,16 +4,27 @@ const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 const getAllJob = async (req, res) => {
-    res.send('get all job')
+    const job = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+    res.status(StatusCodes.OK).json({ job, count: job.length })
+
 }
 const getJob = async (req, res) => {
-    res.send('get  job');
+    const { user: { userId }, params: { id: jobId } } = req;
+
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId
+    })
+    if (!job) {
+        throw new NotFoundError(`No job is there ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job });
 }
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId
     const job = await Job.create(req.body)
-    res.status(StatusCode.CREATED).json({ job })
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
